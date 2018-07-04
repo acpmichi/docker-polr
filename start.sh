0eec0f8245f8bce009b8bafa6d1680326cb2784c
acpmichi <40799523+acpmichi@users.noreply.github.com>
2018-07-04 11:25:35 +0200

    start modified 2

--- start.sh
+++ start.sh
@@ -19,7 +19,12 @@ fi
 if [ ! -f "database/seeds/AdminSeeder.php" ]; then
     envsubst < "AdminSeeder_withoutEnv.php" > "database/seeds/AdminSeeder.php"
 	rm -f AdminSeeder_withoutEnv.php
+	php artisan db:seed --class=AdminSeeder --force
 fi
+
+php artisan migrate --force
+composer dump-autoload
+php artisan geoip:update
 php artisan db:seed --class=AdminSeeder --force
 
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
